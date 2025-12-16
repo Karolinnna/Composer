d003a923c4c9271e670e92b1d51cc61df5a3543f
Karolinnna <karoleti2024@gmail.com>
2025-12-16 12:31:39 +0200

    Update Controller.php

--- laravel/app/Http/Controllers/Controller.php
+++ laravel/app/Http/Controllers/Controller.php
@@ -2,7 +2,27 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+// 1. Необхідні імпорти (Traits)
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController; // Імпорт базового класу Laravel
+
+/**
+ * @abstract
+ * Базовий контролер, від якого успадковуються всі інші контролери
+ * у додатку.
+ */
+abstract class Controller extends BaseController
 {
-    //
+    // 2. Використання Traits
+    // Ці "трейти" (Traits) надають додаткові функції:
+
+    // Надає зручні методи для перевірки прав доступу (Gate/Policies).
+    use AuthorizesRequests;
+
+    // Надає зручні методи для валідації вхідних даних HTTP-запитів.
+    use ValidatesRequests;
+
+    // Тут може бути додана спільна логіка для всіх контролерів,
+    // але зазвичай він залишається порожнім або використовує лише ці трейти.
 }
